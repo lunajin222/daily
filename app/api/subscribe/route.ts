73a020f839a5bef7c57baa46ed0989d2d54c4ad5
lunajin222 <2370379428@qq.com>
2026-09-21 23:06:36 +0800

    feat: 集成 Resend 实现邮件订阅逻辑并添加前端 Toast 提示

--- app/api/subscribe/route.ts
+++ app/api/subscribe/route.ts
@@ -1,5 +1,25 @@
+import { Resend } from 'resend';
+import{ NextResponse } from 'next/server';
 export async function POST(request: Request) {
     const { email } = await request.json();
     console.log(email);
-    return new Response("OK", { status: 200 });
+    const resend = new Resend(process.env.RESEND_API_KEY);
+    //1.create an account
+    const {error: createError } = await resend.contacts.create({
+        email: email,
+    });
+    if (createError) {
+        console.error('Error creating account:', createError);
+        return NextResponse.json({"error": "Error creating account"}, { status: 500 });
+    }
+    //2.add account to contact list
+    const { error: addError } = await resend.contacts.segments.add({
+        email: email,
+        segmentId:"cc3c642a-310f-4137-a574-28a2295712a5",
+    });
+    if (addError) {
+        console.error('Error adding contact:', addError);
+        return NextResponse.json({"error": "Error adding contact"}, { status: 500 });
+    }
+    return NextResponse.json({"message": "Subscribed successfully"}, { status: 200 });
 }
--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import { BookOpenCheck } from "lucide-react";
 import React, { useState } from "react";
 import Card from "@/components/Card";
-
+import { toast } from "sonner"
 export default function Home() {
   const [email, setEmail] =useState('');
 
@@ -13,6 +13,16 @@ export default function Home() {
     fetch('/api/subscribe', {
       method:'POST',
       body: JSON.stringify({ email }),
+    }).then((res=>res.json())).then((data)=>{
+      if(data.error){
+        toast.error(data.error);
+      }else{
+        toast.success('Subscribed successfully');
+      }
+    }).catch((err)=>{
+      toast.error('Error subscribing');
+    }).finally(() => {
+      setEmail('');
     })
   };
   return (
