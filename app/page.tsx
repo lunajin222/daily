f71b2579ad388e29407d0a0df39aeea720b8f1d0
lunajin222 <2370379428@qq.com>
2026-09-21 18:52:51 +0800

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,42 @@
+import Image from "next/image";
+import { BookOpenCheck } from "lucide-react";
+import Card from "@/components/Card";
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-white">
+      {/* header */}
+      <header className="flex items-center justify-between px-8 py-6 max-w-7xl mx-auto">
+        <div className="flex items-center gap-3">
+          <BookOpenCheck/>
+          <h1 className="text-2xl font-bold">Daily News</h1>
+        </div>
+        <nav className="flex items-center gap-6">
+          <a href="/" className="hover:text-gray-600">About</a>
+          <a href="/" className="hover:text-gray-600">Contact</a>
+        </nav>
+      </header>
+      {/* main content */}
+      <div className="max-w-7xl mx-auto px-8 py-12">
+        {/* title */}
+        <div className='text-center mb-16'>
+          <h2 className="text-5xl font-bold mb-6">Welcome to Daily News</h2>
+          <p className="text-lg text-gray-700 max-w-3xl mx-auto leading-relaxed">
+            Stay updated with the latest news and updates from around the world. Our platform provides you with reliable and timely information to keep you informed.
+          </p>
+        </div>
+        {/* input and subscribe button */}
+        <div className='text-center flex items-center justify-center gap-4'>
+          <input type="email" placeholder="Enter your email" className="border border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:ring-2 focus:ring-black focus:border-black" />
+          <button className="bg-black text-white px-6 py-2 rounded-md hover:bg-blue-600 transition-colors">Subscribe</button>
+        </div>
+        {/* cards */}
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mt-12">
+          <Card title="AI" description="Stay updated with the latest AI developments and applications." />
+          <Card title="Startups" description="Get insights into the latest startup news and funding announcements." />
+          <Card title="Tech" description="Get insights into the latest technology trends and innovations." />
+        </div>
+
+      </div>
+    </div>
+  );
+}
---
+++ components/Card.tsx
@@ -0,0 +1,8 @@
+export default function Card({ title, description }: { title: string; description: string}) {
+  return (
+    <div className="bg-white rounded-lg shadow-md p-6 hover:shadow-lg transition-shadow overflow-hidden">
+        <h3 className="text-lg font-semibold mb-4">{title}</h3>
+        <p className="text-gray-700 leading-relaxed">{description}</p>
+    </div>
+  );
+}
